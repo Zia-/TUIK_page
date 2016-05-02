66b4506d49bd01ce9a95a58d6d00960b94fbca1c
Mohammed Zia <mohammed.zia33@gmail.com>
2016-05-02 15:02:51 +0300

    map auto resize working

--- js/sidebar_menu.js
+++ js/sidebar_menu.js
@@ -10,31 +10,37 @@
 // });
 
 $("#wrapper").toggleClass("toggled-2");
+
+var height = $(window).height() - 51;
+$("#map-div").height(height - 40);
 // $('#menu ul').hide();
 
- function initMenu() {
-  // $('#menu ul').hide();
-  $('#menu ul').children('.current').parent().show();
-  //$('#menu ul:first').show();
-  $('#menu li a').click(
-    function() {
-      var checkElement = $(this).next();
-      if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-        return false;
-        }
-      if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-        $('#menu ul:visible').slideUp('normal');
-        checkElement.slideDown('normal');
-        return false;
-        }
-      }
-    );
-  }
-$(document).ready(function() {
-  initMenu();
-  //Subtracting 50px for navbar and for some reasons mapbox adds
-  var height = $(window).height() - 51;
-  $("#sidebar-wrapper").height(height);
-  //40px to the height so subtracting that as well
-  $("#map-div").height(height - 40);
-});
+ // function initMenu() {
+ //  // $('#menu ul').hide();
+ //  $('#menu ul').children('.current').parent().show();
+ //  //$('#menu ul:first').show();
+ //  $('#menu li a').click(
+ //    function() {
+ //      var checkElement = $(this).next();
+ //      if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+ //        return false;
+ //        }
+ //      if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+ //        $('#menu ul:visible').slideUp('normal');
+ //        checkElement.slideDown('normal');
+ //        return false;
+ //        }
+ //      }
+ //    );
+ //  }
+// $(document).ready(function() {
+//   initMenu();
+//   //Subtracting 50px for navbar and for some reasons mapbox adds
+//   var height = $(window).height() - 51;
+//   // $("#sidebar-wrapper").height(height);
+//   // console.log(height);
+//   //40px to the height so subtracting that as well
+//   // $("#map-div").height(height - 40);
+//   // $("#map-div").height(height);
+//   // console.log(height);
+// });
