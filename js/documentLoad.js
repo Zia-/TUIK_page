4cc4b9b37ab3a354c45faa7a85aec7f8c345d799
Mohammed Zia <mohammed.zia33@gmail.com>
2016-05-22 12:30:33 +0300

    print button is working a bit

--- js/documentLoad.js
+++ js/documentLoad.js
@@ -2,3 +2,10 @@ $( document ).ready(function() {
   // This script is to make toolbar button title popup functional
   $(function() {$('.toolbar').tooltip();});
 });
+
+
+
+
+function printMap(){
+  window.print()
+}
